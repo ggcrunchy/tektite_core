da306fde87eeb503a11db17d755bab0678cf2610
Steven Johnson <steve@xibalbastudios.com>
2014-11-15 20:29:06 -0600

    Just a use case to maybe work from, for the ID occupied stuff in the array modules

--- array/id_occupied.lua
+++ array/id_occupied.lua
@@ -32,5 +32,65 @@
 -- Exports --
 local M = {}
 
+--[[
+	-- Update the ID for the current maze and overwrite one slot with an invalid ID. A slot
+	-- is considered "explored" if it contains the current ID. This is a subtle alternative
+	-- to overwriting all slots or building a new table: Since the current ID iterates over
+	-- the number of available slots (and then resets), and since every slot is invalidated
+	-- between occurrences of the ID taking on a given value, all slots will either contain
+	-- some other ID or be invalid, i.e. none will be already explored.
+	local new = array_index.RotateIndex(open.id, #open)
+
+	open.id, open[-new] = new
+
+	-- Compute the deltas between rows of the maze event block (using its width).
+	local col1, col2 = block:GetColumns()
+
+	Deltas[1] = col1 - col2 - 1
+	Deltas[3] = col2 - col1 + 1
+
+	-- Choose a random maze tile and do a random flood-fill of the block.
+	Maze[#Maze + 1] = random(#open / 4)
+
+	repeat
+		local index = Maze[#Maze]
+
+		-- Mark this tile slot as explored.
+		open[-index] = new
+
+		-- Examine each direction out of the tile. If the direction was already marked
+		-- (borders are pre-marked in the relevant direction), or the relevant neighbor
+		-- has already been explored, ignore it. Otherwise, add it to the choices.
+		local oi, n = (index - 1) * 4, 0
+
+		for i, delta in ipairs(Deltas) do
+			if not (open[oi + i] or open[-(index + delta)] == new) then
+				n = n + 1
+
+				Choices[n] = i
+			end
+		end
+
+		-- If there are no choices left, remove the tile from the list. Otherwise, choose
+		-- one of the available directions and mark it, plus the reverse direction in the
+		-- relevant neighbor, and try to resume the flood-fill from that neighbor.
+		if n > 0 then
+			local i = Choices[random(n)]
+			local delta = Deltas[i]
+
+			open[oi + i] = true
+
+			oi = oi + delta * 4
+			i = (i + 1) % 4 + 1
+
+			open[oi + i] = true
+
+			Maze[#Maze + 1] = index + delta
+		else
+			remove(Maze)
+		end
+	until #Maze == 0
+]]
+
 -- Export the module.
 return M
