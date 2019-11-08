6a84d6d2dcf527ba24a2a3c06029060cea2e7cbc
Steven Johnson <steve@xibalbastudios.com>
2019-11-08 14:47:36 -0600

    Sampling now rephrased in terms of SampleSet class

--- number/sampling.lua
+++ number/sampling.lua
@@ -6,7 +6,8 @@
 --
 -- Samples are ordered by increasing **x**. Any parameters not of **number** type must
 -- therefore define **__lt** (and possibly **__le**) metamethods. Sample lookup similarly
--- introduces the need for arithmetic metamethods, q.v. @{Lookup} and @{Lookup_01}.
+-- introduces the need for arithmetic metamethods, q.v. @{SampleSet:Lookup} and
+-- @{SampleSet:Lookup_01}.
 --
 -- Apart from storing and retrieving them, the module ignores the **y** values.
 
@@ -38,13 +39,10 @@ local assert = assert
 local floor = math.floor
 local insert = table.insert
 local remove = table.remove
+local setmetatable = setmetatable
 
 -- Modules --
 local range = require("tektite_core.number.range")
-
--- Cached module references --
-local _GetCount_
-local _Lookup_
 
 -- Exports --
 local M = {}
@@ -99,36 +97,39 @@ local function AuxLookup (samples, n, x, start) -- n as argument to unify stream
 	end
 end
 
---- Adds a new sample to the set, if _x_ is new; otherwise, updates its corresponding sample.
--- @tparam SampleSet samples
+local SampleSet = {}
+
+SampleSet.__index = SampleSet
+
+--- Add a new sample to the set, if _x_ is new; otherwise, updates its corresponding sample.
 -- @tparam Param x Parameter to assign.
 --
 -- If this is not of type **number**, note that an **__eq** metamethod is not necessary to
 -- establish _x_'s uniqueness.
 -- @param y Value to assign.
--- @int[opt] start As per @{Lookup}.
-function M.AddSample (samples, x, y, start)
-	local n, entry, at = _GetCount_(samples)
+-- @int[opt] start As per @{SampleSet:Lookup}.
+function SampleSet:Add (x, y, start)
+	local n, entry, at = self:GetCount()
 
 	-- Append...
-	if n == 0 or x > samples[n].x then
+	if n == 0 or x > self[n].x then
 		at = n + 1
 
 	-- ...prepend...
-	elseif x < samples[1].x then
+	elseif x < self[1].x then
 		at = 1
 
 	-- ...otherwise, insert or update inside the set.
 	else
-		local bin, frac = AuxLookup(samples, n, x, start)
+		local bin, frac = AuxLookup(self, n, x, start)
 
 		-- Matches left side...
 		if frac == 0 then
-			entry = samples[bin]
+			entry = self[bin]
 
 		-- ...or right side...
 		elseif bin == n - 1 and frac == 1 then
-			entry = samples[bin + 1]
+			entry = self[bin + 1]
 
 		-- ...otherwise, within bin.
 		else
@@ -138,39 +139,24 @@ function M.AddSample (samples, x, y, start)
 
 	-- If a new entry is being added, try to recycle an old one. In any case, insert it.
 	if at then
-		entry, samples.n = remove(samples, n + 1) or {}, n + 1
+		entry, self.n = remove(self, n + 1) or {}, n + 1
 
-		insert(samples, at, entry)
+		insert(self, at, entry)
 	end
 
 	-- Assign the fields. Postponing this until now simplifies some lookups above.
 	entry.x, entry.y = x, y
 end
 
---- Accessor.
--- @tparam SampleSet samples
+---
 -- @treturn uint Sample count.
-function M.GetCount (samples)
-	return assert(samples.n, "Uninitialized sampling state")
-end
-
---- Prepares a table for use by the rest of the routines in this module, i.e. afterward
--- _samples_ will be a valid **SampleSet**.
---
--- If _samples_ is already a **SampleSet**, it will be reset.
--- @tparam[opt] ?|table|SampleSet samples Set to prepare. If absent, a table is created.
--- @treturn SampleSet _samples_.
-function M.Init (samples)
-	samples = samples or {}
-
-	samples.n = 0
-
-	return samples
+function SampleSet:GetCount ()
+	return assert(self.n, "Uninitialized sampling state")
 end
 
 -- Given a parameter, gather information about the bin comprising its corresponding samples
 local function FindBin (samples, x, start)
-	local n = _GetCount_(samples)
+	local n = samples:GetCount()
 	local i, frac, oob = AuxLookup(samples, n, x, start)
 
 	return i, frac, oob and (frac == 0 and "<" or ">"), samples[i], i < n and samples[i + 1] -- account for one-sample case
@@ -183,8 +169,7 @@ local function GetFraction (x, entry, next)
 	return next and (x - x1) / (next.x - x1) or 0
 end
 
---- Resolves a parameter to a pair of samples.
--- @tparam SampleSet samples Sample set to search.
+--- Resolve a parameter to a pair of samples.
 -- @ptable result A parameter is resolved to two consecutive samples _s1_ and _s2_ (when only
 -- one sample is available, it is duplicated). These are used to populate the table.
 --
@@ -203,8 +188,8 @@ end
 -- @tparam Param x Parameter to resolve.
 -- @int[opt] start If provided, the index of a sample / bin. If possible, search begins at
 -- this position. This might improve search speed if the correct bin is nearby.
-function M.Lookup (samples, result, x, start)
-	local bin, frac, oob, entry, next = FindBin(samples, x, start)
+function SampleSet:Lookup (result, x, start)
+	local bin, frac, oob, entry, next = FindBin(self, x, start)
 
 	result.bin, result.frac = bin, frac or GetFraction(x, entry, next)
 	result.out_of_bounds = oob or false
@@ -215,39 +200,37 @@ function M.Lookup (samples, result, x, start)
 	result.x2, result.y2 = next.x, next.y
 end
 
---- Variant of @{Lookup} parametrized over a unit interval.
--- @tparam SampleSet samples Sample set to search.
--- @ptable result As per @{Lookup}.
--- @tparam Param x As per @{Lookup}, but the parameter space is remapped to [0, 1]. In
--- particular, an _x_ of 0 or 1 corresponds to the first or last sample, respectively.
+--- Variant of @{SampleSet:Lookup} parametrized over a unit interval.
+-- @ptable result As per @{SampleSet:Lookup}.
+-- @tparam Param x Ditto, but the parameter space is remapped to [0, 1]. In particular, an
+-- _x_ of 0 or 1 corresponds to the first or last sample, respectively.
 --
 -- The **x1** and **x2** in _result_ will have their regular values.
 --
 -- Values are remapped as `x = first + (last - first) * x`, where _first_ and _last_ are the
 -- **x** values in the first and last sample.
--- @int[opt] start As per @{Lookup}.
-function M.Lookup_01 (samples, result, x, start)
-	local last = samples[_GetCount_(samples)]
+-- @int[opt] start Ditto.
+function SampleSet:Lookup_01 (result, x, start)
+	local last = self[self:GetCount()]
 
 	if last then -- If samples are empty, defer assert to lookup step
-		local x1 = samples[1].x
+		local x1 = self[1].x
 
 		x = x1 + (last.x - x1) * x
 	end
 
-	_Lookup_(samples, result, x, start) 
+	self:Lookup(result, x, start) 
 end
 
---- Finds the samples parametrized by _x_, failing if _x_ is out-of-bounds.
--- @tparam SampleSet samples Sample set to search.
+--- Find the samples parametrized by _x_, failing if _x_ is out-of-bounds.
 -- @tparam Param x Parameter to resolve.
--- @int[opt] start As per @{Lookup}.
+-- @int[opt] start As per @{SampleSet:Lookup}.
 -- @treturn ?|uint|boolean If _x_ was out-of-bounds, **false**. Otherwise, the index _i_ of
 -- the bin belonging to samples #_i_ and #_i_ + 1.
 -- @treturn ?number If _x_ was within bounds, its relative position in its bin, as per
--- _result_'s **frac** field in @{Lookup}.
-function M.ToBin (samples, x, start)
-	local i, frac, oob, entry, next = FindBin(samples, x, start)
+-- _result_'s **frac** field in @{SampleSet:Lookup}.
+function SampleSet:ToBin (x, start)
+	local i, frac, oob, entry, next = FindBin(self, x, start)
 
 	if oob then
 		return false
@@ -256,39 +239,37 @@ function M.ToBin (samples, x, start)
 	end
 end
 
---- Variant of @{ToBin} that clamps out-of-bounds parameters.
+--- Variant of @{SampleSet:ToBin} that clamps out-of-bounds parameters.
 --
--- In effect, this is a more minimalist version of @{Lookup}.
--- @tparam SampleSet samples Sample set to search.
+-- In effect, this is a more minimalist version of @{SampleSet:Lookup}.
 -- @tparam Param x Parameter to resolve.
--- @int[opt] start As per @{Lookup}.
--- @treturn uint As per _result_'s **bin** field, in @{Lookup}...
+-- @int[opt] start As per @{SampleSet:Lookup}.
+-- @treturn uint As per _result_'s **bin** field, in @{SampleSet:Lookup}...
 -- @treturn number ...and **frac**...
 -- @treturn ?|string|boolean ...and **out\_of\_bounds**.
-function M.ToBin_Clamped (samples, x, start)
-	local i, frac, oob, entry, next = FindBin(samples, x, start)
+function SampleSet:ToBin_Clamped (x, start)
+	local i, frac, oob, entry, next = FindBin(self, x, start)
 
 	return i, frac or GetFraction(x, entry, next), oob
 end
 
---- Updates the **x** and **y** values of an existing sample.
--- @tparam SampleSet samples
--- @uint index Sample index, &isin; [1, _n_], where _n_ is the sample count, cf. @{GetCount}.
+--- Update the **x** and **y** values of an existing sample.
+-- @uint index Sample index, &isin; [1, _n_], where _n_ is the sample count, cf. @{SampleSet:GetCount}.
 -- @tparam Param x Parameter to assign.
 --
--- A sample can be updated if `samples[index - 1].x < x and x < samples[index + 1].x` is
--- true. In this context, _samples_[0] and _samples_[_n_ + 1] can be interpreted to have
--- values -&inf; and +&inf;, respectively.
+-- A sample can be updated if `samples[index - 1].x < x and x < samples[index + 1].x`. In
+-- this context, _samples_[0] and _samples_[_n_ + 1] can be interpreted to have values -&inf;
+-- and +&inf;, respectively.
 -- @param y Value to assign.
 -- @treturn boolean Could the sample be updated?
 -- @treturn ?Param If the sample was updated, its previous **x** value...
 -- @return ...and **y** value.
--- @see AddSample
-function M.UpdateSample (samples, index, x, y)
-	local n = _GetCount_(samples)
-	local entry = index <= n and samples[index]
+-- @see SampleSet:Add
+function SampleSet:Update (index, x, y)
+	local n = self:GetCount()
+	local entry = index <= n and self[index]
 
-	if entry and (index == 1 or x > samples[index - 1].x) and (index == n or x < samples[index + 1].x) then
+	if entry and (index == 1 or x > self[index - 1].x) and (index == n or x < self[index + 1].x) then
 		entry.x, entry.y, x, y = x, y, entry.x, entry.y
 
 		return true, x, y
@@ -297,7 +278,17 @@ function M.UpdateSample (samples, index, x, y)
 	end
 end
 
-_GetCount_ = M.GetCount
-_Lookup_ = M.Lookup
+--- Prepare a table for use, i.e. afterward _samples_ will be a valid **SampleSet**.
+--
+-- If _samples_ is already a **SampleSet**, it will be reset.
+-- @tparam[opt] ?|table|SampleSet samples Set to prepare. If absent, a table is created.
+-- @treturn SampleSet _samples_.
+function M.New (samples)
+	samples = samples or {}
+
+	samples.n = 0
+
+	return setmetatable(samples, SampleSet)
+end
 
 return M
